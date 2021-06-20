4a94313abcf58714ba98509289cd88e547ec611f
ngocthanhbui <72994444+ngocthanhbui@users.noreply.github.com>
2021-06-20 07:59:32 +0700

    Delete app directory

--- app/src/main/java/CKT/Ministop/Apk/utils/Constants.java
+++
@@ -1,9 +0,0 @@
-package CKT.Ministop.Apk.utils;
-
-public class Constants {
-    public static final String CATEGORY_CHILD = "Categories";
-    public static final String CATEGORY_NAME_CHILD = "categoryName";
-    public static final String ORDER_PRODUCTS_CHILD = "Orders";
-    public static final String USER_CHILD = "User";
-    public static final String USER_ID_CHILD = "userId";
-}
